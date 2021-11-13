3ce7097abb3eb95bcca358e323945ec1cdce319a
None <None>
2021-11-12 22:47:24 -0500

    Added standard project template

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Projects C Category
 Short CS projects; low effort C category
+
+
+
+```python
+from rprojectc import StandardProject
+
+class Application(StandardProject):
+
+    def do(self, a):
+        print('Hello world: {}'.format(a))
+
+```
---
+++ rprojectc/__init__.py
@@ -0,0 +1,18 @@
+import os
+from rutils import ProjectWorker
+
+
+class StandardProject(ProjectWorker):
+    def __init__(self, *args, **kwargs):
+        super(StandardProject, self).__init__(**args, **kwargs)
+        self.init()
+
+    def init(self):
+        homedir = self.config.get("PROJECT_HOME", '.')
+        workdir = os.path.abspath(os.path.join(homedir, 'workdir'))
+
+        if not os.path.exists(workdir):
+            os.makedirs(workdir)
+            self.logger.info('Initialized workdir: {}'.format(workdir))
+        
+        self.config['WORKDIR'] = workdir
